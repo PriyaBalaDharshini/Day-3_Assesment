764e68d5e3a21ecd31bead50348e767e23428812
PriyaBalaDharshini <125110897+PriyaBalaDharshini@users.noreply.github.com>
2023-03-08 12:52:29 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,17 @@
+var resume={
+    "name":"Priyadharshini",
+    "place":"hosur",
+    "contact":"98765432",
+    "mail":"priya@gmail.com",
+    "marks":"95% in 10th--2011",
+            "90% in 12th--2013",
+            "85% in UG--2017" ,
+    "experience":"joined as a trainee in e-pushilishin company",
+                 "worked for the customer RSC for 2 years",
+                 "working for the customer ACS for 2 years as freelancer",
+                 "having 4+ years of experience in pre-editing",
+    "hobbies":"playing volleyball",
+    "strength":"can easily mingle with the co-worker",
+    "weakness":"slow learner"             
+};
+console.log(resume);
