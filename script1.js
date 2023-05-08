824cec0b8c8f727ab1343c7a8985e579a7604803
PriyaBalaDharshini <125110897+PriyaBalaDharshini@users.noreply.github.com>
2023-05-08 15:43:43 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -17,6 +17,6 @@ for(var i in bussiness){
 var info=[{"name":"priya","age":"27","gender":"femele","occupation":"freelancer","contact":"12345"},
 {"name":"sham","age":"20","gender":"mele","occupation":"staff","contact":"23456"},
 {"name":"pavi","age":"25","gender":"femele","occupation":"engineer","contact":"34567"}];
-for(var i = 0; i<=info.length; i++){
+for(var i = 0; i<info.length; i++){
     console.log(info[i].name,info[i].age,info[i].gender,info[i].occupation,info[i].contact);
 }
